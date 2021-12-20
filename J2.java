2339f4a1712731d70d25cbc12426673b2a3ff9cb
甘愷翔 <92702961+kaihsiang610@users.noreply.github.com>
2021-12-20 12:15:25 +0800

    Add files via upload

---
+++ J2.java
@@ -0,0 +1,43 @@
+interface iShape03 {
+	public void show();
+
+	public void area();
+}
+
+interface iColor {
+	public void showColor();
+}
+
+class CWin implements iShape03 {
+	protected int width;
+	protected int height;
+	protected String color;
+
+	public CWin(int w, int h, String str) {
+		width = w;
+		height = h;
+		color = str;
+	}
+
+	public void show() {
+		System.out.println("width=" + width);
+		System.out.println("height=" + height);
+	}
+
+	public void area() {
+		System.out.println("area=" + width * height);
+	}
+
+	public void showColor() {
+		System.out.println("color=" + color);
+	}
+}
+
+public class J2 {
+	public static void main(String args[]) {
+		CWin win = new CWin(5, 7, "Green");
+		win.showColor();
+		win.show();
+		win.area();
+	}
+}
